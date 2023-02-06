04348fefb3a9839320c4f3446022178f037ab96d
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-02-06 12:46:25 +0100

    Added exception

--- src/main/java/com/biit/rest/exceptions/UnprocessableEntityException.java
+++ src/main/java/com/biit/rest/exceptions/UnprocessableEntityException.java
@@ -1,13 +1,17 @@
 package com.biit.rest.exceptions;
 
 public class UnprocessableEntityException extends Exception {
-	private static final long serialVersionUID = 1375636360322118631L;
+    private static final long serialVersionUID = 1375636360322118631L;
 
-	public UnprocessableEntityException(String message) {
-		super(message);
-	}
-	
-	public UnprocessableEntityException(String message, Throwable e) {
-		super(message, e);
-	}
+    public UnprocessableEntityException(String message) {
+        super(message);
+    }
+
+    public UnprocessableEntityException(String message, Throwable e) {
+        super(message, e);
+    }
+
+    public UnprocessableEntityException(Throwable e) {
+        super(e);
+    }
 }
